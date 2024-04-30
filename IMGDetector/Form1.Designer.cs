009d421f75646e18522f281ce5c9a9a92fe23e13
Patbodin Intajak <zpatbodin.i@pttdigital.com>
2024-04-30 16:24:17 +0700

    Add col: NeedFixing including its calculation

--- IMGDetector/Form1.Designer.cs
+++ IMGDetector/Form1.Designer.cs
@@ -34,6 +34,9 @@
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // textBox1
@@ -61,7 +64,8 @@
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
-            this.columnHeader3});
+            this.columnHeader3,
+            this.columnHeader4});
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(42, 308);
             this.listView1.Name = "listView1";
@@ -85,11 +89,36 @@
             this.columnHeader3.Text = "Length";
             this.columnHeader3.Width = 100;
             // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(82, 13);
+            this.textBox2.MaxLength = 0;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(131, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Limit:";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "NeedFixing";
+            this.columnHeader4.Width = 100;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1085, 665);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -110,6 +139,9 @@
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
     }
 }
 
--- IMGDetector/Form1.cs
+++ IMGDetector/Form1.cs
@@ -15,15 +15,19 @@ namespace IMGDetector
 {
     public partial class Form1 : Form
     {
+        private double limitChar = Math.Pow(2.0, 15.0);
         public Form1()
         {
             InitializeComponent();
             //listView1.Columns.Add("No", 10, HorizontalAlignment.Left);
             //listView1.Columns.Add("Image Source", 30, HorizontalAlignment.Left);
+
+            textBox2.Text = limitChar.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             listView1.Items.Clear();  // Clear previous items
 
             // Load the HTML from the TextBox
@@ -41,8 +45,13 @@ namespace IMGDetector
                 {
                     // Extract the src attribute
                     string src = img.GetAttributeValue("src", string.Empty);
+                    string[] splitSrc = src.Split(',');
                     //MessageBox.Show(src.Substring(0, 30));
-                    string[] itemStr = new string[] { (count++).ToString() , src.Split(',').Count() == 2 ? src.Split(',')[1] : "NO DATA", src.Split(',').Count() == 2 ? src.Split(',')[1].Length.ToString() : "NO DATA" };
+                    string[] itemStr = new string[] { (count++).ToString(), 
+                        splitSrc.Count() > 1 ? splitSrc[1] : "NO DATA", 
+                        splitSrc.Count() > 1 ? splitSrc[1].Length.ToString() : "NO DATA",
+                        splitSrc[1].Length > limitChar ? "True" : ""
+                    };
                     // Add the src to the ListView
                     ListViewItem item = new ListViewItem(itemStr);
                     
