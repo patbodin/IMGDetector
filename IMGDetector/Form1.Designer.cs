7d64e087cf0d922800140827013bd5dccc0d4c1a
Patbodin Intajak <zpatbodin.i@pttdigital.com>
2024-04-30 16:10:35 +0700

    Allow the app to detect and then add items into ListView

--- IMGDetector/Form1.Designer.cs
+++ IMGDetector/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // textBox1
@@ -40,12 +43,12 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(604, 228);
+            this.textBox1.Size = new System.Drawing.Size(995, 228);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(689, 57);
+            this.button1.Location = new System.Drawing.Point(962, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -55,18 +58,38 @@
             // 
             // listView1
             // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(42, 308);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(604, 97);
+            this.listView1.Size = new System.Drawing.Size(995, 310);
             this.listView1.TabIndex = 2;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "No";
+            this.columnHeader1.Width = 50;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Image";
+            this.columnHeader2.Width = 500;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Length";
+            this.columnHeader3.Width = 100;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1085, 665);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -84,6 +107,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
 
--- IMGDetector/Form1.cs
+++ IMGDetector/Form1.cs
@@ -18,8 +18,8 @@ namespace IMGDetector
         public Form1()
         {
             InitializeComponent();
-            listView1.Columns.Add("No", -2, HorizontalAlignment.Left);
-            listView1.Columns.Add("Image Source", -1, HorizontalAlignment.Left);
+            //listView1.Columns.Add("No", 10, HorizontalAlignment.Left);
+            //listView1.Columns.Add("Image Source", 30, HorizontalAlignment.Left);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,14 +36,16 @@ namespace IMGDetector
             if (images != null)
             {
                 //MessageBox.Show(images.Count().ToString());
+                int count = 1;
                 foreach (var img in images)
                 {
                     // Extract the src attribute
                     string src = img.GetAttributeValue("src", string.Empty);
-                    MessageBox.Show(src.Substring(0, 30));
-                    string[] itemStr = new string[] { src.Substring(0, 30), "test" };
+                    //MessageBox.Show(src.Substring(0, 30));
+                    string[] itemStr = new string[] { (count++).ToString() , src.Split(',').Count() == 2 ? src.Split(',')[1] : "NO DATA", src.Split(',').Count() == 2 ? src.Split(',')[1].Length.ToString() : "NO DATA" };
                     // Add the src to the ListView
                     ListViewItem item = new ListViewItem(itemStr);
+                    
                     listView1.Items.Add(item);
                 }
             }
